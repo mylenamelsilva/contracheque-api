44389c6523b99d2b2b88f7897220d336085e72b5
mylena mel <mylenamsfarias@gmail.com>
2024-09-03 21:30:35 -0300

    feat: criação da model e conexão ao banco de dados

---
+++ src/Business/Users/Model/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Business.Users.Model;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Sobrenome { get; set; } = null!;
+
+    public string Documento { get; set; } = null!;
+
+    public string Setor { get; set; } = null!;
+
+    public decimal SalarioBruto { get; set; }
+
+    public DateOnly DataAdmissao { get; set; }
+
+    public bool DescontoPlanoSaude { get; set; }
+
+    public bool DescontoPlanoDental { get; set; }
+
+    public bool DescontoValeTransporte { get; set; }
+}
---
+++ src/Infra/DatabaseContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Users.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infra;
+
+public partial class DatabaseContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DbConexao"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC07CC19B0EF");
+
+            entity.HasIndex(e => e.Documento, "UQ__Usuarios__AF73706D90F4C10C").IsUnique();
+
+            entity.Property(e => e.Documento)
+                .HasMaxLength(11)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Nome)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.SalarioBruto).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.Setor)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Sobrenome)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
