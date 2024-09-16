d645f657c0916cc03e7131dfbfc0c589f112ca13
mylena mel <mylenamsfarias@gmail.com>
2024-09-16 11:31:22 -0300

    feat: enum e model de lançamento criado

---
+++ src/Business/Contracheque/Enums/TipoLancamentoEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contracheque.Enums
+{
+    public enum TipoLancamentoEnum
+    {
+        Nenhum = 0,
+        Desconto,
+        Remuneracao
+    }
+}
---
+++ src/Business/Contracheque/Models/Lancamento.cs
@@ -0,0 +1,29 @@
+﻿using Business.Contracheque.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contracheque.Models
+{
+    public class Lancamento
+    {
+        public static readonly string DescricaoInss = "INSS mês";
+        public static readonly string DescricaoImposto = "IRRF mês";
+        public static readonly string DescricaoValeTransporte = "Vale Transporte mês";
+        public static readonly string DescricaoPlanoDental = "Plano Dental mês";
+        public static readonly string DescricaoPlanoSaude = "Plano de Saúde mês";
+
+        public TipoLancamentoEnum Tipo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+
+        public Lancamento(TipoLancamentoEnum tipo, string descricao, decimal valor)
+        {
+            Tipo = tipo;
+            Descricao = descricao;
+            Valor = valor;
+        }
+    }
+}
